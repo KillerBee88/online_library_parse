c9b7272684cd04a22f3c9d3aab003772c376cd31
KillerBee88 <hardmen08@gmail.com>
2023-06-13 22:26:37 +0300

    Update manage.py
    Added command line arguments support.

--- manage.py
+++ manage.py
@@ -1,8 +1,16 @@
+import argparse
 import os
+
 import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
-from urllib.parse import urljoin, urlsplit, unquote
+from urllib.parse import urljoin
+
+
+def get_book_page(book_url):
+    response = requests.get(book_url)
+    response.raise_for_status()
+    return response.text
 
 
 def parse_book_page(book_url):
@@ -34,107 +42,85 @@ def get_book_genre(book_id):
 
     soup = BeautifulSoup(response.text, 'lxml')
 
-    book_info_container = soup.find('div', id='content')
-
-    genre = book_info_container.find('span', class_='d_book').find('a').get_text(strip=True)
+    genre = soup.find('span', class_='d_book').find('a').get_text(strip=True)
 
     return genre
 
 
-def download_comments(book_id, filename):
+def download_txt(txt_url, filename, folder='books'):
+    response = requests.get(txt_url, allow_redirects=False)
+    response.raise_for_status()
+
+    os.makedirs(folder, exist_ok=True)
+
+    with open(os.path.join(folder, filename), 'w') as file:
+        file.write(response.text)
+
+
+def download_image(img_url, filename, folder='images'):
+    response = requests.get(img_url, allow_redirects=False)
+    response.raise_for_status()
+
+    os.makedirs(folder, exist_ok=True)
+
+    with open(os.path.join(folder, filename), 'wb') as file:
+        file.write(response.content)
+
+
+def download_comments(book_id, filename, folder='comments'):
     url = f'https://tululu.org/b{book_id}/'
     response = requests.get(url)
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'lxml')
 
-    comments_containers = soup.find_all('div', class_='texts')
-
-    if not comments_containers:
-        return
-    
-    comments = [container.find('span', 'black') for container in comments_containers]
-
-    with open(filename, 'a', encoding='utf-8') as file:
-        for i, comment in enumerate(comments):
-            comment_text = comment.get_text(separator="\n")
-            file.write(f'Комментарий {i+1}: {comment_text}\n')
-
-
-def download_txt(url, filename, folder='books/'):
-    sanitized_filename = sanitize_filename(filename)
+    comments = soup.find_all('div', class_='texts')
 
     os.makedirs(folder, exist_ok=True)
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-    response = requests.get(url, headers=headers)
-
-    if response.history:
-        if response.url.startswith('https://tululu.org/txt.php'):
-            raise requests.HTTPError('Redirected to the main page')
-    content_type = response.headers.get('Content-Type')
-    if not content_type.startswith('text/plain'):
-        return None
-
-    filepath = os.path.join(folder, f'{sanitized_filename}.txt')
-    with open(filepath, 'wb') as file:
-        file.write(response.content)
-
-    return filepath
+    with open(os.path.join(folder, filename), 'w') as file:
+        for comment in comments:
+            file.write(comment.get_text(strip=True) + '\n\n')
 
 
-def download_image(url, filename, folder='images/'):
-    sanitized_filename = unquote(urlsplit(url).path.split('/')[-1])
+def main():
+    parser = argparse.ArgumentParser(description='Download books from tululu.org')
+    parser.add_argument('start_id', type=int, default=1, help='Start book ID')
+    parser.add_argument('end_id', type=int, default=10, help='End book ID')
+    args = parser.parse_args()
 
-    os.makedirs(folder, exist_ok=True)
+    start_id = args.start_id
+    end_id = args.end_id
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-    response = requests.get(url, headers=headers)
+    for book_id in range(start_id, end_id + 1):
+        book_url = f"https://tululu.org/b{book_id}/"
+        response = requests.get(book_url)
 
-    if response.history:
-        if response.url.startswith('https://tululu.org/txt.php'):
-            raise requests.HTTPError('Redirected to the main page')
-    content_type = response.headers.get('Content-Type')
-    if not content_type.startswith('image/'):
-        return None
+        soup = BeautifulSoup(response.text, "lxml")
+        title_and_author = soup.find("h1").text
+        split_title_and_author = title_and_author.split("::")
 
-    filepath = os.path.join(folder, f'{filename}.jpg')
-    with open(filepath, 'wb') as file:
-        file.write(response.content)
+        if len(split_title_and_author) == 1:
+            continue
 
-    return filepath
+        title, author = split_title_and_author
+        title = title.strip()
+        author = author.strip()
+        genre = get_book_genre(book_id)
 
-os.makedirs('books', exist_ok=True)
-os.makedirs('images', exist_ok=True)
-os.makedirs('comments', exist_ok=True)
+        txt_url = f'https://tululu.org/txt.php?id={book_id}'
+        txt_filename = f'{book_id}_{sanitize_filename(title)}.txt'
+        img_url = urljoin(book_url, soup.find('div', class_='bookimage').find('img')['src'])
+        img_filename = f'{book_id}_{sanitize_filename(title)}.jpg'
+        comments_filename = f'{book_id}_{sanitize_filename(title)}.txt'
 
-for book_id in range(1, 11):
-    book_url = f"https://tululu.org/b{book_id}/"
-    response = requests.get(book_url)
+        book_data = parse_book_page(book_url)
 
-    soup = BeautifulSoup(response.text, "lxml")
-    title_and_author = soup.find("h1").text
-    split_title_and_author = title_and_author.split("::")
+        download_comments(book_id, comments_filename)
+        download_txt(txt_url, txt_filename)
+        download_image(img_url, img_filename)
+        print(book_data)
 
-    if len(split_title_and_author) == 1:
-        continue
 
-    title, author = split_title_and_author
-    title = title.strip()
-    author = author.strip()
-    genre = get_book_genre(book_id)
-
-    txt_url = f'https://tululu.org/txt.php?id={book_id}'
-    txt_filename = f'{book_id}_{sanitize_filename(title)}'
-    img_url = urljoin(book_url, soup.find('div', class_='bookimage').find('img')['src'])
-    img_filename = f'{book_id}_{sanitize_filename(title)}'
-    comments_filename = f'{book_id}_{sanitize_filename(title)}.txt'
-
-    book_data = parse_book_page(book_url)
-
-    download_comments(book_id, os.path.join('comments', comments_filename))
-    download_txt(txt_url, txt_filename)
-    download_image(img_url, img_filename)
-    print(book_data)
+if __name__ == '__main__':
+    main()
