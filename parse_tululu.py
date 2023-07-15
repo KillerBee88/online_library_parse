c9ce2b1f663203cf58257dcf22d7d8e1e3422ff2
KillerBee88 <hardmen08@gmail.com>
2023-07-15 16:09:56 +0300

    Update parse_tululu.py
    Добавил отладочные принты для проверки срабатывания условий if

--- parse_tululu.py
+++ parse_tululu.py
@@ -100,7 +100,10 @@ def main():
         html = response.text
         book_description = parse_book_page(html, book_url)
         if book_description is None:
+            print(f"Для книги {book_id} описание не найдено")
             continue
+        else:
+            print(f"Для книги {book_id} описание получено")
 
         txt_url = f'https://tululu.org/txt.php?id={book_id}'
         txt_filename = f'{book_id}_{sanitize_filename(book_description["Name"])}.txt'
@@ -110,7 +113,10 @@ def main():
 
         comments = get_comments(html)
         if comments:
+            print(f"Для книги {book_id} комментарии получены")
             save_comments(comments, comments_filename)
+        else:
+            print(f"Для книги {book_id} комментариев нет")
 
         download_txt(txt_url, txt_filename)
         download_image(img_url, img_filename) 
