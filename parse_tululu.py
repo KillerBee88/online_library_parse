c2ff7fe2c657558886228c5fa57158929641ba89
KillerBee88 <hardmen08@gmail.com>
2023-07-24 21:06:38 +0300

    Update parse_tululu.py

--- parse_tululu.py
+++ parse_tululu.py
@@ -22,25 +22,23 @@ def parse_book_page(html, book_url):
 
     genre = soup.select_one('span.d_book a').text.strip()
 
-    img_url = soup.select_one('div.bookimage img')['src']
-    img_url = urljoin(book_url, img_url)
+    img_url = urljoin(book_url, soup.select_one('div.bookimage img')['src'])
 
-    book_description = {
+    comments = [comment.get_text(strip=True) for comment in soup.find_all('div', class_='texts')]
+
+    return {
         'Name': title,
         'Author': author,
         'Genre': genre,
-        'Cover': img_url
+        'Cover': img_url,
+        'Comments': comments
     }
-
-    comments = soup.find_all('div', class_='texts')
-    book_description['Comments'] = [comment.get_text(strip=True) for comment in comments]
-
-    return book_description
 
 
 def download_txt(txt_url, filename, params=None, folder='books'):
     response = requests.get(txt_url, params=params)
     os.makedirs(folder, exist_ok=True)
+    
     with open(os.path.join(folder, filename), 'w', encoding='utf-8') as file:
         file.write(response.text)
 
@@ -49,16 +47,16 @@ def download_txt(txt_url, filename, params=None, folder='books'):
 def download_image(img_url, filename, folder='images'):
     response = requests.get(img_url)
     os.makedirs(folder, exist_ok=True)
+    
     with open(os.path.join(folder, filename), 'wb') as file:
         file.write(response.content)
 
 
 def save_comments(comments, filename, folder='comments'):
     os.makedirs(folder, exist_ok=True)
-
+    
     with open(os.path.join(folder, filename), 'w', encoding='utf-8') as file:
-        for comment in comments:
-            file.write(comment + '\n\n')
+        file.writelines(comment + '\n\n' for comment in comments)
 
 
 def main():
@@ -66,10 +64,10 @@ def main():
     parser.add_argument('start_id', type=int, default=1, help='Start book ID')
     parser.add_argument('end_id', type=int, default=10, help='End book ID')
     args = parser.parse_args()
-
+    
     start_id = args.start_id
     end_id = args.end_id
-
+    
     for book_id in range(start_id, end_id + 1):
         book_url = f"https://tululu.org/b{book_id}/"
         try:
@@ -82,7 +80,7 @@ def main():
             print(f"Ошибка подключения к {book_url}: {e}", file=sys.stderr)
             time.sleep(5)
             continue
-
+        
         html = response.text
         book_description = parse_book_page(html, book_url)
         if book_description is None:
@@ -90,27 +88,26 @@ def main():
             continue
         else:
             print(f"Для книги {book_id} описание получено")
-
+            
         txt_url = 'https://tululu.org/txt.php'
         txt_params = {'id': book_id}
         txt_filename = f'{book_id}_{sanitize_filename(book_description["Name"])}.txt'
         img_url = book_description['Cover']
         img_filename = f'{book_id}_{sanitize_filename(book_description["Name"])}.jpg'
         comments_filename = f'{book_id}_{sanitize_filename(book_description["Name"])}.txt'
-
+        
         comments = book_description.get('Comments')
         if comments:
             print(f"Для книги {book_id} комментарии получены")
             save_comments(comments, comments_filename)
         else:
             print(f"Для книги {book_id} комментариев нет")
-
         try:
             download_txt(txt_url, txt_filename, txt_params)
         except requests.exceptions.RequestException as e:
             print(f'Ошибка при выполнении запроса для книги {book_id}: {e}', file=sys.stderr)
             continue
-
+        
         try:
             download_image(img_url, img_filename)
         except requests.exceptions.RequestException as e:
